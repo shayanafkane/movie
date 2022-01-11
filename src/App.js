61d9f18f26e14e389433263bd1935597d610d465
shayanafkane <shayanafkane@gmail.com>
2022-01-11 15:39:24 +0330

    Add-nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import styled from "styled-components";
 
+const Container = styled.div`
+  display : flex;
+  flex-direction: column;
+`;
+const Header = styled.div`
+display : flex;
+flex-direction : row;
+background-color : black;
+color : white ;
+padding : 10px ;
+font-size : 25px;
+box-shadow : 0 3px 6px 0 #555
+`;
 
 function App() {
   return (
-    <div className="App">
-     Shayan
-    </div>
+    
+    <Container>
+      <Header>React Movie Mobile</Header>
+      Shayan
+    </Container>
   );
 }
 
